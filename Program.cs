9f625a59c7fa5412f1e66b98cef48052e5674adc
Daniel Taylor <djt09d@fulbrightmail.org>
2017-09-17 16:39:05 -0600

    Anonymous Methods

--- Program.cs
+++ Program.cs
@@ -6,30 +6,75 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
-    public delegate void WorkPerformedHandler(int hours, WorkType workType);
 
     class Program
     {
-
         static void Main(string[] args)
         {
-            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
-            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
 
-            del1 += del2;
+            //del1 += del2 + del3;
 
-            del1(5, WorkType.Golf);
+            //DoWork(del1);
+
+            //  int finalHours = del1(10, WorkType.GoToMeetings);
+            //Console.WriteLine(finalHours);
+
+            var worker = new Worker();
+            //worker.WorkPerformed += new EventHandler<WorkPerfomedEventArgs>(worker_WorkPerformed);
+            //worker.WorkCompleted += new EventHandler(worker_WorkCompleted);
+
+            worker.WorkPerformed += Worker_WorkPerformed;
+
+            worker.WorkPerformed += delegate (object sender, WorkPerfomedEventArgs e)
+            {
+                Console.WriteLine("Anonymous Method");
+            };
+
+            worker.WorkCompleted += Worker_WorkCompleted;
+
+
+            worker.DoWork(8, WorkType.GenerateReports);
+
             Console.ReadLine();
         }
 
-        static void WorkPerformed2(int hours, WorkType workType)
+        private static void Worker_WorkCompleted(object sender, EventArgs e)
         {
-            Console.WriteLine("WorkPerformed " + workType + " and hours" + (hours * 1.2));
+            Console.WriteLine("Work Complete");
+
         }
 
-        static void WorkPerformed1(int hours, WorkType workType)
+        private static void Worker_WorkPerformed(object sender, WorkPerfomedEventArgs e)
+        {
+            Console.WriteLine("Hours worked" + e.Hours + " " + e.WorkType);
+        }
+
+
+        //static void DoWork(WorkPerformedHandler del)
+        //{
+        //    del(5, WorkType.GoToMeetings);
+        //}
+
+        static int WorkPerformed1(int hours, WorkType workType)
         {
             Console.WriteLine("WorkPerformed 1 ");
+
+            return hours + 1;
+        }
+        
+        static int WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed " + workType + " and hours" + (hours * 1.2));
+            return hours + 2;
+
+        }
+        static int WorkPerformed3(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed 3 called " + hours.ToString());
+            return hours + 3;
         }
 
     }
---
+++ Worker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    //public delegate int WorkPerformedHandler(object sender, WorkPerfomedEventArgs e);
+
+
+    public class Worker
+    {
+        //public event WorkPerformedHandler WorkPerformed;
+
+        public event EventHandler<WorkPerfomedEventArgs> WorkPerformed;
+        public event EventHandler WorkCompleted; 
+        
+        public void DoWork(int hours, WorkType workType)
+        {
+            for(int i = 0; i < hours; i++)
+            {
+                //raise event
+                System.Threading.Thread.Sleep(1000);
+                OnWorkPerformed(i + 1, workType);
+            }
+
+            //raise workcompleted
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            var del = WorkPerformed;
+            if(del != null)
+            {
+                del(this, new WorkPerfomedEventArgs(hours, workType));
+            }
+        }
+        protected virtual void OnWorkCompleted()
+        {
+            var del = WorkCompleted as EventHandler;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
+        }
+    }
+}
