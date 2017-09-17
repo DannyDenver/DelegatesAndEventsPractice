c0a491b546ec827d46e807d044b0d3df8111096f
Daniel Taylor <djt09d@fulbrightmail.org>
2017-09-17 13:07:37 -0600

    first Delegate

--- Program.cs
+++ Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
     class Program
     {
+
         static void Main(string[] args)
         {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            del1 += del2;
+
+            del1(5, WorkType.Golf);
+            Console.ReadLine();
         }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed " + workType + " and hours" + (hours * 1.2));
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed 1 ");
+        }
+
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
     }
 }
